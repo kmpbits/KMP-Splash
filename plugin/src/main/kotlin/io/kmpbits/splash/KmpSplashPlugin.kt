0f98c10fd9da1a104869a39daf011d6f51c45c23
KMP Bits <kmpbits@gmail.com>
2026-05-09 22:53:13 +0100

    Fix task registration: wrap lambda with Action<T> to avoid constructor injection

--- plugin/src/main/kotlin/io/kmpbits/splash/KmpSplashPlugin.kt
+++ plugin/src/main/kotlin/io/kmpbits/splash/KmpSplashPlugin.kt
@@ -2,6 +2,7 @@ package io.kmpbits.splash
 
 import io.kmpbits.splash.tasks.GenerateAndroidSplashTask
 import io.kmpbits.splash.tasks.GenerateLaunchScreenTask
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -20,22 +21,24 @@ class KmpSplashPlugin : Plugin<Project> {
         val iosPath = ext.iosProjectPath.orNull
             ?: error("KmpSplash: iosProjectPath must be set in the splashScreen { } block")
 
-        val task = project.tasks.register("generateLaunchScreen", GenerateLaunchScreenTask::class.java, { t: GenerateLaunchScreenTask ->
-            t.group = "kmp-splash"
-            t.description = "Generates LaunchScreen.storyboard for iOS"
+        val task = project.tasks.register(
+            "generateLaunchScreen",
+            GenerateLaunchScreenTask::class.java,
+            Action<GenerateLaunchScreenTask> { t ->
+                t.group = "kmp-splash"
+                t.description = "Generates LaunchScreen.storyboard for iOS"
 
-            t.backgroundColor.set(ext.backgroundColor)
-            t.outputFile.set(project.rootProject.file("$iosPath/LaunchScreen.storyboard"))
+                t.backgroundColor.set(ext.backgroundColor)
+                t.outputFile.set(project.rootProject.file("$iosPath/LaunchScreen.storyboard"))
 
-            ext.logoFile.orNull?.let { logoPath ->
-                val logoFile = project.file(logoPath)
-                val resourceName = logoFile.nameWithoutExtension
-                t.logoResourceName.set(resourceName)
-                t.logoSourceFile.set(logoFile)
+                ext.logoFile.orNull?.let { logoPath ->
+                    val logoFile = project.file(logoPath)
+                    t.logoResourceName.set(logoFile.nameWithoutExtension)
+                    t.logoSourceFile.set(logoFile)
+                }
             }
-        })
+        )
 
-        // Hook into the Xcode build lifecycle when the Apple framework task is present.
         project.tasks.configureEach {
             if (name == "embedAndSignAppleFrameworkForXcode") {
                 dependsOn(task)
@@ -44,22 +47,25 @@ class KmpSplashPlugin : Plugin<Project> {
     }
 
     private fun registerAndroidTask(project: Project, ext: KmpSplashExtension) {
-        val task = project.tasks.register("generateAndroidSplash", GenerateAndroidSplashTask::class.java, { t: GenerateAndroidSplashTask ->
-            t.group = "kmp-splash"
-            t.description = "Generates Android splash screen themes.xml"
+        val task = project.tasks.register(
+            "generateAndroidSplash",
+            GenerateAndroidSplashTask::class.java,
+            Action<GenerateAndroidSplashTask> { t ->
+                t.group = "kmp-splash"
+                t.description = "Generates Android splash screen themes.xml"
 
-            t.backgroundColor.set(ext.backgroundColor)
-            t.backgroundColorNight.set(ext.backgroundColorNight)
+                t.backgroundColor.set(ext.backgroundColor)
+                t.backgroundColorNight.set(ext.backgroundColorNight)
 
-            // Default to androidMain/res inside the applying module.
-            t.resOutputDir.set(project.file("src/androidMain/res"))
+                t.resOutputDir.set(project.file("src/androidMain/res"))
 
-            ext.logoFile.orNull?.let { logoPath ->
-                val logoFile = project.file(logoPath)
-                t.logoDrawableName.set(logoFile.nameWithoutExtension)
-                t.logoSourceFile.set(logoFile)
+                ext.logoFile.orNull?.let { logoPath ->
+                    val logoFile = project.file(logoPath)
+                    t.logoDrawableName.set(logoFile.nameWithoutExtension)
+                    t.logoSourceFile.set(logoFile)
+                }
             }
-        })
+        )
 
         project.tasks.configureEach {
             if (name == "preBuild") {
