c829c7fe2cc1dbaa0f988f4ab7dc4abcdfb2b53f
KMP Bits <kmpbits@gmail.com>
2026-05-10 01:21:37 +0100

    Remove SplashScreen expect/actual: keep only iOS implementation

--- runtime/src/iosMain/kotlin/io/kmpbits/splash/SplashScreen.ios.kt
+++ runtime/src/iosMain/kotlin/io/kmpbits/splash/SplashScreen.ios.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 
 @Composable
-actual fun SplashScreen(
+fun SplashScreen(
     isReady: suspend () -> Boolean,
     onFinished: () -> Unit,
     backgroundColor: Color,
