1d17c5c02b24cd4208a8b57de5aa99bb5329936d
KMP Bits <kmpbits@gmail.com>
2026-07-15 16:57:12 +0100

    Update sample: add delay to isReady and configure slide-up exit animation

--- sample/androidApp/src/main/kotlin/io/kmpbits/splash/sample/androidapp/MainActivity.kt
+++ sample/androidApp/src/main/kotlin/io/kmpbits/splash/sample/androidapp/MainActivity.kt
@@ -2,10 +2,14 @@ package io.kmpbits.splash.sample.androidapp
 
 import android.widget.TextView
 import io.kmpbits.splash.SplashActivity
+import kotlinx.coroutines.delay
 
 class MainActivity : SplashActivity() {
 
-    override suspend fun isReady(): Boolean = true
+    override suspend fun isReady(): Boolean {
+        delay(1500)
+        return true
+    }
 
     override fun onFinished() {
         setContentView(TextView(this).apply { text = "KMP Splash — androidApp sample" })
